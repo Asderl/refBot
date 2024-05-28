292063b18f4c2b2520c35a16b1a458a74f0e268b
Asderl <vaargreptiloid@gmail.com>
2024-05-28 17:15:19 +0700

    Added referrals count to /ref message, added commentaries, reduced number of database operations

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 from aiogram.utils.deep_linking import decode_payload, create_start_link
 from aiogram.filters import Command, CommandStart, CommandObject
 from aiogram import Bot, Dispatcher, Router
-from aiogram.types import Message, User
-from aiogram.enums import ParseMode
+from aiogram.types import Message
 from bot_token import TOKEN
 from db import Database
 import asyncio
@@ -10,30 +9,41 @@ import asyncio
 router = Router()
 dp = Dispatcher()
 
-table_name = "main"
-db = Database("database", "referals")
-bot = Bot(TOKEN)
+table_name = "main" #Name of table in database
+db = Database("database", "referals") #Database path is ./database/referals
+bot = Bot(TOKEN) #Telegram bot token
 
+#Handler for /start command with payload
 @router.message(CommandStart(deep_link=True))
 async def command_start_dl(message: Message, command: CommandObject) -> None:
     args = command.args
     payload = decode_payload(args)
-    if await db.add_new_user("main", str(message.from_user.id), payload) != -1:
-        await db.add_referal("main", payload)
+    #Check is user id different than in referral link
+    if payload == str(message.from_user.id):
+        await message.answer("You can't be referral of yourself")
+        await db.add_new_user(table_name, str(message.from_user.id))
+        return
+    #Check is user not already presented in table
+    if await db.add_new_user(table_name, str(message.from_user.id), payload) != -1:
+        await db.add_referal(table_name, payload)
 
+#Handler for /start command without payload
 @router.message(Command("start"))
 async def command_start(message: Message) -> None:
-    await db.add_new_user("main", str(message.from_user.id))
+    await db.add_new_user(table_name, str(message.from_user.id))
 
+#Handler for /ref command - show referral count and referral link
 @router.message(Command("ref"))
 async def command_referal(message: Message) -> None:
     ref_link = await create_start_link(bot, message.from_user.id, encode=True)
-    await message.answer(f"Your referal link: {ref_link}")
+    ref_count = await db.get_ref_count(table_name, str(message.from_user.id))
+    await message.answer(f"Your referrals: {ref_count}\nYour referral link: {ref_link}")
 
-
+#Main function - start bot polling
 async def main() -> None:
     dp.include_router(router)
     await dp.start_polling(bot)
 
+#Entry point
 if __name__ == "__main__":
     asyncio.run(main())
--- db.py
+++ db.py
@@ -4,6 +4,10 @@ from log import Logger
 from os import path
 
 class Database:
+    """Class Database provides asynchronous access to sqlite database
+
+    Params: relative path to database file, each directory in separate string
+    """
     def __init__(self, *table_path: str) -> None:
         db_path = path.dirname(__file__)
         for file in table_path:
@@ -11,9 +15,9 @@ class Database:
         self.db_path = db_path
         self.logger = Logger("db")
 
-    async def add_new_user(self, table_name: str, user_id: str, parent: str = 0) -> None|int:
-        values: list[str] = [i[0] for i in await self.get_all_users("main")]
-        if user_id in values:
+    #Add new user to specified table of database, user don't have parent by default
+    async def add_new_user(self, table_name: str, user_id: str, parent: str = "0") -> None|int:
+        if await self.get_user(table_name, user_id) == user_id:
             self.logger.info(f"user {user_id} already presented!")
             return -1
         else:
@@ -22,6 +26,17 @@ class Database:
             await db.commit()
             await db.close()
 
+    #Get user from specified table of database
+    async def get_user(self, table_name: str, user_id: str) -> str|None:
+        db = await asql.connect(self.db_path)
+        cursor = await db.execute(f"SELECT user_id FROM {table_name} WHERE user_id={user_id}")
+        value = await cursor.fetchone()
+        await db.close()
+        if value != None:
+            return value[0]
+        return None
+
+    #Get all users from specified table of database
     async def get_all_users(self, table_name: str) -> Iterable[asql.Row]:
         db = await asql.connect(self.db_path)
         cursor = await db.execute(f"SELECT * FROM {table_name}")
@@ -29,9 +44,9 @@ class Database:
         await db.close()
         return values
 
+    #Get user parent
     async def get_parent(self, table_name: str, user_id: str) -> str|None:
-        values: list[str] = [i[0] for i in await self.get_all_users("main")]
-        if user_id in values:
+        if await self.get_user(table_name, user_id) == user_id:
             db = await asql.connect(self.db_path)
             cursor = await db.execute(f"SELECT parent FROM {table_name} WHERE user_id = {user_id}")
             parent = await cursor.fetchone()
@@ -39,8 +54,17 @@ class Database:
             self.logger.debug(parent[0])
             return parent[0]
 
+    #Increase user referral count
     async def add_referal(self, table_name: str, parent: str) -> None:
         db = await asql.connect(self.db_path)
         await db.execute(f"UPDATE {table_name} SET referals = referals + 1 WHERE user_id = {parent}")
         await db.commit()
         await db.close()
+
+    #Get user referral count
+    async def get_ref_count(self, table_name: str, user_id: str) -> int:
+        db = await asql.connect(self.db_path)
+        cursor = await db.execute(f"SELECT referals FROM {table_name} WHERE user_id = {user_id}")
+        ref_count = await cursor.fetchone()
+        await db.close()
+        return ref_count[0]
